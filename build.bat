6ac823a3607f37fc68649ccff089805e9b14fb98
xsoder <tadihailukebe@gmail.com>
2026-01-19 02:47:34 -0800

    windows clang fix

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-clang.exe -std=c99 -Wall -Wextra -O2 ccalc.c -o ccalc -lm -DBUILD_DIR=$(pwd)
+clang.exe -Wno-deprecated-declarations -Wall -Wextra -O2 ccalc.c -o ccalc.exe -DBUILD_DIR=$(pwd)
--- example/simple-c/build.bat
+++ example/simple-c/build.bat
@@ -2,6 +2,4 @@
 
 mkdir build
 
-clang.exe -shared -fPIC -o build/add.dll add.c
-
-echo "Usage run the ./ccalc.exe add.calc"
+clang.exe -shared -o build/add.dll add.c
