bac65b2836031157b06a058d1c70ed44ae943853
xsoder <tadihailukebe@gmail.com>
2026-01-26 12:42:36 -0500

    Fixing win build

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-clang.exe -std=c99 -Wno-deprecated-declarations -Wall -Wextra -O2 aoxim.c -o aoxim.exe -ldl -lm -DBUILD_DIR=%%pwd%
+clang.exe -std=c99 -Wno-deprecated-declarations -Wall -Wextra -O2 aoxim.c -o aoxim.exe -DBUILD_DIR=%%pwd%
